7efb399d6fa276666bcc9404423f376596c638c2
Bryan Brito <bryan.brito.062@ufrn.edu.br>
2021-07-27 19:30:44 -0300

    Mudanças monolito

---
+++ monolito/SpringBoot-Angular7-Online-Shopping-Store/backend/src/main/java/me/zhulin/shopapi/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,49 @@
+package me.zhulin.shopapi.service.impl;
+
+
+import me.zhulin.shopapi.entity.ProductCategory;
+import me.zhulin.shopapi.enums.ResultEnum;
+import me.zhulin.shopapi.exception.MyException;
+import me.zhulin.shopapi.repository.ProductCategoryRepository;
+import me.zhulin.shopapi.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created By Zhu Lin on 3/10/2018.
+ */
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    ProductCategoryRepository productCategoryRepository;
+
+    @Override
+    public List<ProductCategory> findAll() {
+        return productCategoryRepository.findAllByOrderByCategoryType();
+    }
+
+    @Override
+    public ProductCategory findByCategoryType(Integer categoryType) {
+        ProductCategory res = productCategoryRepository.findByCategoryType(categoryType);
+
+        if(res == null) {
+            throw new MyException(ResultEnum.CATEGORY_NOT_FOUND);
+        }
+
+        return res;
+    }
+
+    @Override
+    public List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList) {
+        return productCategoryRepository.findByCategoryTypeInOrderByCategoryTypeAsc(categoryTypeList);
+    }
+
+    @Override
+    @Transactional
+    public ProductCategory save(ProductCategory productCategory) {
+        return productCategoryRepository.save(productCategory);
+    }
+}
